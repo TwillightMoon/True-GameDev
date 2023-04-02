df3c18dded02b5c28eb8b38b7f413c2ac7717d4a
PaleLuna <robertmkrtyan334@gmail.com>
2023-04-03 01:04:33 +0300

    Перенос главной сцены, добавление башен

--- Tower Defense/Assets/Scripts/GameGridScripts/DefensivePosition.cs
+++ Tower Defense/Assets/Scripts/GameGridScripts/DefensivePosition.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class DefensivePosition : MonoBehaviour
 {
+    [SerializeField] private Color _selectedColor;
+    [SerializeField] private bool _isSelected;
+
     [SerializeField] private OrientationEnum _blockOrientation = OrientationEnum.Up;
     private Buildings building;
+
+    private SpriteRenderer[] _childsSpriteRenderers;
+    private BoxCollider2D _boxCollider2D;
 
     public Buildings GetBuilding() => building;
     public bool isOccupied => building != null;
@@ -16,22 +23,42 @@ public class DefensivePosition : MonoBehaviour
         }
 
         Quaternion rotation = Quaternion.Euler(Orientation.GetVectorRotation(_blockOrientation));
-        building = Instantiate(buildingPrefab, transform.position, rotation, parent);
+        building = Instantiate(buildingPrefab, (Vector2)transform.position + _boxCollider2D.offset, rotation, parent);
 
         OnDeselected();
     }
 
     public void OnSelected()
     {
-        GetComponent<SpriteRenderer>().color = Color.red;
         if (!building)
             UIEvents.SendSelectedEmptyBlock(this);
         else
             UIEvents.SendSelectedOccupiedBlock(this);
+
+        _isSelected = true;
+        ChangeColor();
     }
     public void OnDeselected()
     {
-        GetComponent<SpriteRenderer>().color = Color.white;
         UIEvents.SendDeselectBlock();
+
+        _isSelected = false;
+        ChangeColor();
+    }
+
+
+    private void Start()
+    {
+        _childsSpriteRenderers = transform.GetComponentsInChildren<SpriteRenderer>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void ChangeColor()
+    {
+        Color currentColor = _isSelected ? _selectedColor : Color.white;
+        foreach (SpriteRenderer childSpriteRenderer in _childsSpriteRenderers)
+        {
+            childSpriteRenderer.color = currentColor;
+        }
     }
 }
