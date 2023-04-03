a9dacbbf02ef5fca527fa04ddd0774cb8f8ed42d
PaleLuna <palelunadeveloper@gmail.com>
2023-04-03 11:58:21 +0300

    Рефактор Barracs

--- Tower Defense/Assets/Scripts/BuildingsScript/Tower/TowerInfo/TowerTypes/Barracks.cs
+++ Tower Defense/Assets/Scripts/BuildingsScript/Tower/TowerInfo/TowerTypes/Barracks.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "AreaTower", order = 4, menuName = "Gameplay/Towers/New Barracks")]
+[CreateAssetMenu(fileName = "Barracs", order = 4, menuName = "Gameplay/Towers/New Barracks")]
 public class Barracks : BuildingsConfig
 {
     [Header("Характеристики юнитов")]
     [SerializeField][Tooltip("Количество юнитов одновременно")] private byte _unitCount;
     [SerializeField][Tooltip("Время на восстановление одного юнита в секундах")] private uint _respawnTime;
-
 }
