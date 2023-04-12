75f3e9040b945749d7c8b1283fcef0f32ed9fab7
PaleLuna <palelunadeveloper@gmail.com>
2023-04-12 14:02:17 +0300

    Подготовка почвы для реализации юнитов

--- Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/BuildingsScript/Modules/EnemyDetector.cs
+++ Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/BuildingsScript/Modules/EnemyDetector.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Unit.EnemyScrips;
 
 namespace Buildings
 {
--- Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/BuildingsScript/Tower/TowerState/TowerCombat.cs
+++ Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/BuildingsScript/Tower/TowerState/TowerCombat.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using Unit.EnemyScrips;
 
 namespace Buildings
 {
