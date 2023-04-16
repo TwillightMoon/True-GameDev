656ed5748949e43ffd7131131dc7d1f0423ab59d
PaleLuna <palelunadeveloper@gmail.com>
2023-04-16 17:12:31 +0300

    Переосмысление комментарией

--- Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/BuildingsScript/Building.cs
+++ Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/BuildingsScript/Building.cs
@@ -2,38 +2,37 @@ using UnityEngine;
 using ConfigClasses.BuildingConfig;
 using Buildings.TowerStates;
 
-/** Пространство имен классов, что относятся к постройкам.
- *  К этому пространству имён относятся классы, наследующиеся от Building, а также модули, дополняющие поведение построек.
- */
 namespace Buildings
 {
-    /** Родительский класс для всех построек.
-    * Класс определяет базовый набор полей и методов, которые являются общими для всех построек.
-    */
+
+    ///<summary>
+    /// Родительский класс для всех построек.
+    /// Класс определяет базовый набор полей и методов, которые являются общими для всех построек.
+    /// </summary>
     [RequireComponent(typeof(Rigidbody2D))]
     [RequireComponent(typeof(SpriteRenderer))]
     public abstract class Building : MonoBehaviour, IStateChange, IInteractable
     {
         [Header("Компоненты")]
-        [SerializeField] protected EnemyDetector _enemyDetector; /**< EnemyDetector variable. Компонент, отвечающий за обнаружение врагов. */
-        [SerializeField] protected CombatRadiusVisualizer _combatRadiusVisualizer /**< CombatRadiusVisualizer variable. Компонент, отвечающий за визуализацию радиуса. */;
+        [SerializeField] protected EnemyDetector m_enemyDetector; /// <value>EnemyDetector. Компонент, отвечающий за обнаружение врагов.</value>
+        [SerializeField] protected CombatRadiusVisualizer m_combatRadiusVisualizer; /// <value>CombatRadiusVisualizer. Компонент, отвечающий за визуализацию радиуса.</value>
 
-        protected Rigidbody2D _rigidbody2D /**< Rigidbody2D variable. Компонент, отвечающий за физическую обработку. */;
-        protected SpriteRenderer _spriteRenderer /**< SpriteRenderer variable. Компонент, отвечающий за отображение графики объекта. */;
+        protected Rigidbody2D m_rigidbody2D; /// <value>Rigidbody2D. Компонент, отвечающий за физическую обработку.</value> 
+        protected SpriteRenderer m_spriteRenderer; /// <value>SpriteRenderer. Компонент, отвечающий за отображение графики объекта.</value> 
 
 
         [Header("Характеристика постройки")]
-        [SerializeField] protected BuildingsConfig _buildingCharacteristic /**< BuildingsConfig variable. Компонент, хранящий основыне хар-ки постройки. */;
+        [SerializeField] protected BuildingsConfig _buildingCharacteristic; /// <value>BuildingsConfig. Компонент, хранящий основыне хар-ки постройки.</value>
 
         [Header("Характеристика постройки на каждом уровне")]
-        private int _currentLevelIndex = 0 /**< integer variable. Индекс текущего уровня постройки */;
-        [SerializeField] protected BuildingsConfig[] _towerLevels /**< integer[] variable. Массив уровней построки. */;
-
+        private int _currentLevelIndex = 0; /**< integer variable. Индекс текущего уровня постройки */
+        [SerializeField] protected BuildingsConfig[] _towerLevels; /**< integer[] variable. Массив уровней построки. */
+        
         [Header("Состояния постройки")]
         [SerializeField] protected TowerState[] towerStates; /**< TowerState[] variable. Массив состояний построки. */
-        protected TowerState currentState /**< TowerState variable. Текущее состояние */;
+        protected TowerState currentState; /**< TowerState variable. Текущее состояние */
 
-        public EnemyDetector enemyDetector => _enemyDetector;
+        public EnemyDetector enemyDetector => m_enemyDetector;
         public BuildingsConfig buildingsConfig => _buildingCharacteristic;
 
         /**
@@ -41,10 +40,10 @@ namespace Buildings
         */
         protected void Init()
         {
-            _rigidbody2D = GetComponent<Rigidbody2D>();
-            _spriteRenderer = GetComponent<SpriteRenderer>();
+            m_rigidbody2D = GetComponent<Rigidbody2D>();
+            m_spriteRenderer = GetComponent<SpriteRenderer>();
 
-            _rigidbody2D.bodyType = RigidbodyType2D.Static;
+            m_rigidbody2D.bodyType = RigidbodyType2D.Static;
 
             SortLevels();
             SetNewCharacteristics();
@@ -85,18 +84,18 @@ namespace Buildings
         */
         private void SetNewCharacteristics()
         {
-            if (_enemyDetector)
-                _enemyDetector.combatRadius = _buildingCharacteristic.combatRadius;
+            if (m_enemyDetector)
+                m_enemyDetector.combatRadius = _buildingCharacteristic.combatRadius;
             else
                 Debug.LogError("EnemyDetector не установлен!");
 
-            if (_spriteRenderer)
-                _spriteRenderer.sprite = _buildingCharacteristic.towerSprite;
+            if (m_spriteRenderer)
+                m_spriteRenderer.sprite = _buildingCharacteristic.towerSprite;
             else
                 Debug.LogError("SpriteRenderer не установлен!");
 
-            if (_combatRadiusVisualizer)
-                _combatRadiusVisualizer.SetLine(_buildingCharacteristic.combatRadius);
+            if (m_combatRadiusVisualizer)
+                m_combatRadiusVisualizer.SetLine(_buildingCharacteristic.combatRadius);
             else
                 Debug.LogError("CombatRadiusVisualizer не установлен!");
         }
@@ -152,16 +151,16 @@ namespace Buildings
         */
         public void OnSelected()
         {
-            if (_combatRadiusVisualizer)
-                _combatRadiusVisualizer.ActiveLine(true);
+            if (m_combatRadiusVisualizer)
+                m_combatRadiusVisualizer.ActiveLine(true);
         }
         /** Реализация контракта IInteractable.
         * Метод, выполняющийся при отмене выделения постойки игроком.
         */
         public void OnDeselected()
         {
-            if (_combatRadiusVisualizer)
-                _combatRadiusVisualizer.ActiveLine(false);
+            if (m_combatRadiusVisualizer)
+                m_combatRadiusVisualizer.ActiveLine(false);
         }
     }
 }
