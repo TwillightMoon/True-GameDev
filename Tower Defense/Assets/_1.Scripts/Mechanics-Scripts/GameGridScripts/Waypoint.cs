0281cfed60acd4577d652aa6247c9e5637a19874
PaleLuna <robertmkrtyan334@gmail.com>
2023-06-04 20:24:51 +0300

    Связи между точками

--- Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/GameGridScripts/Waypoint.cs
+++ Tower Defense/Assets/_1.Scripts/Mechanics-Scripts/GameGridScripts/Waypoint.cs
@@ -1,3 +1,4 @@
+using DebugScripts.GizmosDebug;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
@@ -12,4 +13,12 @@ public class Waypoint : MonoBehaviour
         return _childrends[index];
     }
     public int childCount => _childrends.Length;
+
+    private void OnDrawGizmos()
+    {
+        if (_childrends == null || childCount <= 0) return;
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < childCount; i++)
+            Gizmos.DrawLine(transform.position, _childrends[i].transform.position);
+    }   
 }
